773580f67de530ef2fa30c860272345e5f99fd54
隔壁家的老黄 <944364674@qq.com>
2017-11-07 16:26:44 +0800

    Update treeGrid2.0.vue

--- src/components/treeGrid2.0.vue
+++ src/components/treeGrid2.0.vue
@@ -175,7 +175,7 @@ export default {
         },
         // 点击事件 返回数据处理
         makeData(data) {
-            const t = typeOf(data);
+            const t = type(data);
             let o;
             if (t === 'array') {
                 o = [];
